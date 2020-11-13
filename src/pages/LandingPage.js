eef3bc2bce82629145779e83504f65597d2a6f86
fajaramaulana <59821005+fajaramaulana@users.noreply.github.com>
2020-11-13 23:48:48 +0700

    component part mostpicked,categories finished

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,43 +1,55 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import SideDrawer from 'parts/sidedrawer/SideDrawer'
-import Header from 'parts/Header/Header'
-import landingPage from 'json/landingPage.json'
-import Hero from 'parts/Hero.js'
-import Backdrop from 'parts/Backdrop/Backdrop';
+import SideDrawer from "parts/sidedrawer/SideDrawer";
+import Header from "parts/Header/Header";
+import landingPage from "json/landingPage.json";
+import Hero from "parts/Hero.js";
+import MostPicked from "parts/MostPicked";
+import Categories from "parts/Categories";
+import Backdrop from "parts/Backdrop/Backdrop";
 export default class LandingPage extends Component {
-    state = {
-        sideDrawerOpen: false,
+  constructor(props) {
+    super(props);
+    this.refMostPicked = React.createRef();
+  }
+  state = {
+    sideDrawerOpen: false,
+  };
+
+  drawerToggleClickHandler = () => {
+    this.setState((prevState) => {
+      return { sideDrawerOpen: !prevState.sideDrawerOpen };
+    });
+  };
+
+  backdropClickHandler = () => {
+    this.setState({ sideDrawerOpen: false });
+  };
+
+  render() {
+    let backdrop;
+
+    if (this.state.sideDrawerOpen) {
+      backdrop = <Backdrop click={this.backdropClickHandler} />;
     }
-    
-    drawerToggleClickHandler = () => {
-    this.setState(prevState => {
-            return {sideDrawerOpen: !prevState.sideDrawerOpen};
-        });
-    }
+    return (
+      <div style={{ height: "100%" }}>
+        <Header
+          drawerClickHandler={this.drawerToggleClickHandler}
+          {...this.props}
+        ></Header>
+        <SideDrawer show={this.state.sideDrawerOpen} />
+        {backdrop}
 
-    backdropClickHandler = () => {
-        this.setState({ sideDrawerOpen: false })
-      }
-
-    render() {
-        let backdrop;
-
-        if(this.state.sideDrawerOpen){
-            backdrop = <Backdrop click={this.backdropClickHandler}/>
-        }
-        return (
-            <div style={{height: '100%'}}>
-            <Header drawerClickHandler={this.drawerToggleClickHandler} {...this.props}></Header>
-            <SideDrawer show={this.state.sideDrawerOpen}/>
-            {backdrop}
-
-            <main>
-            <Hero data={landingPage.hero}/>
-            </main>
-            
-                
-            </div>
-        )
-    }
+        <main>
+          <Hero refMostPicked={this.refMostPicked} data={landingPage.hero} />
+          <MostPicked
+            refMostPicked={this.refMostPicked}
+            data={landingPage.mostPicked}
+          />
+          <Categories data={landingPage.categories} />
+        </main>
+      </div>
+    );
+  }
 }
